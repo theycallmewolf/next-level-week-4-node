af1ac7e904a9307f1130ce310dcc617801afecbf
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-24 20:26:55 +0000

    initial commit with express

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(3333, () => {
+  console.log(`
+  _______________________
+
+  Server is Up!
+  @TheyCallMeWolf
+
+  _______________________
+  `)
+});
