445e5b94834c5a5cd7ea722f4b39cb38f920342e
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-27 22:11:36 +0000

    adds survey tests

---
+++ src/__tests__/Survey.test.ts
@@ -0,0 +1,34 @@
+import request from 'supertest';
+import { app } from '../app';
+import createConnection from '../database';
+
+
+describe('Surveys', () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  it('should be able to create a new survey', async () => {
+    const response = await request(app).post('/surveys')
+    .send({
+      title: 'title example',
+      description: 'description example',
+    })
+    
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('id');
+  })
+  
+  it('should be able to get all surveys', async () => {
+    await request(app).post('/surveys')
+    .send({
+      title: 'title example 2',
+      description: 'description example 2',
+    })
+    
+    const response = await request(app).get('/surveys');
+
+    expect(response.body.length).toBe(2);
+  })
+})
