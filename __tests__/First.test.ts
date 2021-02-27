1669d41dbe08c04e08da8c513f61bbba07681650
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-27 21:17:43 +0000

    setup test enviroment

---
+++ __tests__/First.test.ts
@@ -0,0 +1,10 @@
+describe('First', () => {
+
+  it('should be possible to add 2 numbers', () => {
+    expect(2 + 2).toBe(4);
+  });
+  
+  it('should be possible to add 2 numbers', () => {
+    expect(2 + 2).toBe(5);
+  });
+});
