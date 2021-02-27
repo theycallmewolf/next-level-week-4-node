873fd72e4367bf05b58d50da8310bc0e985108b6
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-27 13:49:03 +0000

    adds routes.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
+import { router } from './database/routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.status(200).json({ message: 'hello wolf' });
-}); 
-
-app.post('/', (request, response) => {
-  return response.status(200).json({message: 'dados recebidos com sucesso'})
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => {
   console.log(`
