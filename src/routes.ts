ca474daef27a9cfb47d17f07d178839f8d027f4a
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-27 14:48:31 +0000

    fix route file location

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { UserController } from './controllers/UserController';
 
 const router = Router();
 
-const userController = new UserController;
+const userController = new UserController();
 
 router.post('/users', userController.create);
 
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
-import { router } from './database/routes';
+import { router } from './routes';
 
 const app = express();
 
