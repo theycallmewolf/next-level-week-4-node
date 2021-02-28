e23e577d305f3848ce99ea37c4926d65916b0979
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-28 11:18:36 +0000

    fix findOne query filters from or to and

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -42,10 +42,10 @@ class SendMailController {
     }
 
     const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-      where: [
-        {user_id : user.id},
-        {value: null},
-      ],
+      where: [{
+          user_id : user.id,
+          value: null
+        }],
       relations: ['user', 'survey'],
     })
 
