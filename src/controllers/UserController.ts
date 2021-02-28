d96609b53bf571fb48ce6dc49cc2793e164cffaa
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-28 12:44:58 +0000

    add validations to create user request

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,29 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
+import * as yup from 'yup';
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required('Nome obrigatório'),
+      email: yup.string().email('Adicione um email válido').required('Email obrigatório'),
+    });
     
+    // validation version 1
+    // if(!(await schema.isValid(request.body))) {
+    //   return response.status(400).json({error: 'validation failed'});
+    // }
+    
+    // validation version 2
+    try {
+      await schema.validate(request.body, {abortEarly: false});
+    } catch ({errors}) {
+        return response.status(400).json({error: errors});
+    }
+
     // repository -> entity manager (link to db)
     const usersRepository = getCustomRepository(UsersRepository);
     
